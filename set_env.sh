09e022a2f067aa3e2a711e6d2728bd02700b3680
miriamsafwat2 <miriam.safwat@gmail.com>
2022-12-26 23:12:57 +0200

    Update travis.yaml

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=adminadmin
+export POSTGRES_PASSWORD=adminadmin
+export POSTGRES_HOST=project3.czrbfqnh7n74.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=test-709142703476-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
